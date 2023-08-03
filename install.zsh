eea8bb6969c938ea631b4cc9647fe21c365a57cb
bigint <bigint@lenster.xyz>
2023-08-03 19:23:00 +0530

    chore: update dotfiles

---
+++ install.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git config --global user.name "bigint"
+git config --global user.email "69431456+bigint@users.noreply.github.com"
--- path.zsh
+++
@@ -1,3 +0,0 @@
-export PATH="$DOTFILES/bin:$PATH"
-export PATH="$HOME/.node/bin:$PATH"
-export PATH="node_modules/.bin:vendor/bin:$PATH"
